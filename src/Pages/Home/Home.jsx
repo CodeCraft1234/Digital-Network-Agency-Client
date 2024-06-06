431244ddc02c82d65b6cf3c9ea84e1be09b3bc07
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-06-06 14:31:47 +0600

    banner design started

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Banner from "../../Components/Banner/Banner";
 import Carousel from "../../Components/Carousel/Caousel";
 import Payment from "../../Components/Payment/Payment";
 import Service from "../../Components/Services/Services";
@@ -7,6 +8,7 @@ import Cards from "../../components/Cards/Cards";
 const Home = () => {
     return (
         <div className="mt-36">
+            <Banner></Banner>
             <Service></Service>
             <Cards></Cards>
             <Carousel></Carousel>
